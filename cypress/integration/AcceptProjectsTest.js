8610219b000c7ea408d28c4b40eee5b432f3105b
EOleynik <Malbox@i.ua>
2020-08-30 17:36:28 +0300

    change

--- cypress/integration/AcceptProjectsTest.js
+++ cypress/integration/AcceptProjectsTest.js
@@ -1,14 +1,7 @@
-import loginPage from "../elements/LoginPage";
+
 import projectsPage from "../elements/ProjectsPage";
 
 describe('Accept Projects', () => {
-
-    // beforeEach('', () => {
-    //     loginPage.visit('/');
-    //     loginPage.getManageAuthorization();
-    //     loginPage.getButtonToAdmibPanel().click()
-
-    //})
 
     it('Accept Project', () => {
         projectsPage.acceptProject();

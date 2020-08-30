96a99437401cc461faa38120d906ec63c5764fd1
EOleynik <Malbox@i.ua>
2020-08-30 17:46:59 +0300

    commit

--- cypress/integration/VerificationTests.spec.js
+++ cypress/integration/VerificationTests.spec.js
@@ -1,5 +1,4 @@
 import loginPage from "../elements/LoginPage";
-import user from "../fixtures/user.json";
 import homePage from "../elements/HomePage";
 import persVerPage from "../elements/PersVerPage";
 import businessVerPage from "../elements/BusinessVerPage";
@@ -33,7 +32,7 @@ import businessVerPage from "../elements/BusinessVerPage";
                 persVerPage.sendRequestIban();
             })
 
-            it.only('Business Verification', () => {
+            it('Business Verification', () => {
                 loginPage.getButtonToAdmibPanel().click();
                 businessVerPage.sendCompanyInfo();
                 businessVerPage.sendRequestIban();
@@ -47,36 +46,6 @@ import businessVerPage from "../elements/BusinessVerPage";
 
 
 
-
-
-        // loginPage.visit('/');
-        // loginPage.getEmailField().type(user.email);
-        // loginPage.getPasswordField().type(user.password);
-        // loginPage.getButtonLogin().click();
-        // loginPage.getAuthCode().type(user.authCode);
-        //
-        // cy.wait(2000);
-        // homePage.getCheckUrl();
-        // cy.wait(2000);
-        // //homePage.getChangeAccount();
-        // homePage.getMenuVerification().click();
-        // cy.wait(2000);
-        // homePage.getSubmenuPersonal().click();
-        // persVerPage.getCheckUrl();
-        // persVerPage.getFullNameField().type('Jack Jonson');
-        // persVerPage.getBirthdayField();      // type('2000.02.02');
-        // persVerPage.getPassportField().type('CV123456')
-        // persVerPage.getCheckboxPassportWithoutExpirationDate().click();
-        // persVerPage.getCheckBoxMyAddressIsDisplayedOnThePassport().click();
-        // persVerPage.getChoiceCountry();
-        // persVerPage.getCityField().type('Avoca');
-        // persVerPage.getZIPField().type('12345');
-        // persVerPage.getAddressField().type('78 Canberra Avenue, Griffith, ACT 2603');
-        // persVerPage.getCheckBoxTermsAndConditions().click();
-        // persVerPage.getButtonNext().click();
-        //
-         //persVerPage.getUploadYourself();
-        // persVerPage.getUpload();
 
 
 

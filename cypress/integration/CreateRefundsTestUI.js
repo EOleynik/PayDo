cf69147f9f2abd2813e434a8db6307f745bb42f7
EOleynik <Malbox@i.ua>
2020-09-08 12:32:36 +0300

    createRefunds test was adde

---
+++ cypress/integration/CreateRefundsTestUI.js
@@ -0,0 +1,59 @@
+
+import loginPage from "../elements/LoginPage";
+import homePage from "../elements/HomePage";
+import transactionsPage from "../elements/TransactionsPage";
+import createCheckoutPage from "../elements/CreateCheckoutPage"
+
+describe('Refund suit', () => {
+
+    beforeEach('', () => {
+        loginPage.visit('/');
+        loginPage.getAuthorization();
+        createCheckoutPage.getCheckout2API(300);
+    })
+
+    // Частичный рефанд на 100 USD, цена товара 300 USD
+    it('Partial Refund', () => {
+        loginPage.getButtonToAdmibPanel().click();
+        cy.wait(2000);
+        homePage.getCheckUrl();
+        cy.wait(2000);
+        homePage.getMenuTransactions().click();
+        transactionsPage.getButtonDetails().click();
+        transactionsPage.getButtonPartialRefund().click();
+        transactionsPage.getInputPartialRefundAmount().type('100');
+        transactionsPage.getButtonCreateRefund().click();
+        transactionsPage.getButtonOk().click();
+        transactionsPage.checkCreateRefund();
+    })
+
+    // Полный рефанд, цена товара 300 USD
+    it('Full Refund', () => {
+        loginPage.getButtonToAdmibPanel().click();
+        cy.wait(2000);
+        homePage.getCheckUrl();
+        cy.wait(2000);
+        homePage.getMenuTransactions().click();
+        transactionsPage.getButtonDetails().click();
+        transactionsPage.getButtonRefund().click();
+        transactionsPage.confirmRefund().click();
+        transactionsPage.getButtonOk().click();
+        transactionsPage.checkCreateRefund();
+    })
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+

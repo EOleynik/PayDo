9894ce0570af264bb7a2f2849330986b6444990b
EOleynik <Malbox@i.ua>
2020-09-08 16:01:02 +0300

    commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,12 +18,3 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
-    // `on` is used to hook into various events Cypress emits
-    // `config` is the resolved Cypress config
-}
-

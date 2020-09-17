e392d6dcf12acb15c6159a62beff6a13cc3ce5e1
EOleynik <Malbox@i.ua>
2020-09-17 15:34:06 +0300

    double commands "remove captcha" was deleted and added commands "parse"

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -36,7 +36,7 @@ Cypress.Commands.add('form_request', (method, url, formData, done) => {
         done(xhr);
     };
     xhr.send(formData);
-})
+});
 
 Cypress.Commands.add('uploadFile', { prevSubject: true }, (subject, fixturePath, mimeType) => {
     cy.fixture(fixturePath, 'base64').then(content => {
@@ -51,10 +51,6 @@ Cypress.Commands.add('uploadFile', { prevSubject: true }, (subject, fixturePath,
             cy.wrap(subject).trigger('change', { force: true });
         });
     });
-})
-
-Cypress.Commands.add("remove_captcha",()=>{
-    localStorage.setItem('disable-captcha', true);
 });
 
 Cypress.Commands.add("remove_captcha",()=>{
@@ -63,4 +59,12 @@ Cypress.Commands.add("remove_captcha",()=>{
 
 Cypress.Commands.add("generateClient",() => {
     Cypress.env('current_client',Math.random().toString(36).substring(3))
+});
+
+Cypress.Commands.add("parse",(string) => {
+
+    var el = document.createElement( 'html' );
+    el.innerHTML = parse(string).toString();
+    return el;
+
 });

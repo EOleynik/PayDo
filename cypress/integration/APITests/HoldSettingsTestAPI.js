6365ca3e56710982936f16b62af01625cc1def80
EOleynik <Malbox@i.ua>
2020-10-04 11:55:48 +0300

    Commit

--- cypress/integration/APITests/HoldSettingsTestAPI.js
+++ cypress/integration/APITests/HoldSettingsTestAPI.js
@@ -1,15 +1,15 @@
 
-import feenpage from "../elements/FeenPage"
+import feenpage from "../../elements/FeenPage"
 
 describe('Hold settings', () => {
 
     // On group Cards International
     it('Set payment settings ', () => {
         feenpage.setPaymentSettings();
-    })
+    });
 
     // Set Hold of Pending balance [0 days 100%] for PM 300
-    it('Hold seting ', () => {
+    it('Hold setting ', () => {
         feenpage.changeHoldPendingBalance();
     })
-})
+});

ac19fc0ff38963ca2e388569efbe9e127768bcbf
EOleynik <Malbox@i.ua>
2020-11-23 17:58:11 +0200

    Refund math test API

--- cypress/integration/APITests/RefandMathTestsAPI.js
+++ cypress/integration/APITests/RefandMathTestsAPI.js
@@ -1,35 +1,66 @@
 import transactionsPage from "../../elements/TransactionsPage";
 import createCheckoutPage from "../../elements/CreateCheckoutPage";
-import refundPage from "../../elements/RefundPage"
+import refundPage from "../../elements/RefundPage";
+import refund from "../../fixtures/refund";
 
 cy.getRandomArbitrary = function getRandomArbitrary(min, max) {
     return (Math.random() * (max - min) + min).toFixed(2);
 };
 
-    describe('Refunds', () => {
+ describe('Refund math', () => {
 
-        // 1. Цена товара - совпадает с валютами которые в кабинете мерчанта (USD,EUR,GBP, RUB) и валютами провайдера, Full refund.
-        // Валюта товара - USD, основная валюта мерчанта - USD, валюта ПМ - USD
-        it('Full refund', () => {
+     it('Full refund, payment currency matches the currencies that are in the merchant account', () => {
 
-            let payAmount = cy.getRandomArbitrary(100, 500);
+         let payAmount = cy.getRandomArbitrary(100, 500);
+         //let payAmount = 500;
 
-           createCheckoutPage.getCheckout2API(payAmount);
-           cy.wait(3000);
-           transactionsPage.createFullRefundAndCheckAmount(payAmount);
-        });
+         for (let i = 0; i < refund.currency.length; i++) {
+             let payCurrency = refund.currency[i];
+             cy.log(payCurrency);
+             createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+             cy.wait(3000);
+             transactionsPage.createFullRefundAndCheckAmount(payAmount, payCurrency);
+             refundPage.rejectRefund();
+         }
+     });
 
-        //
-        it('Partial refund', () => {
-           let payAmount =cy.getRandomArbitrary(100, 500);
+     it('Full refund, payment currency does not match the currencies that are in the merchant account', () => {
 
-           createCheckoutPage.getCheckout2API(payAmount);
-           cy.wait(3000);
-           transactionsPage.createPartialRefundAndCheckAmount(payAmount);
-        });
+         let payAmount = cy.getRandomArbitrary(100, 500);
+         //let payAmount = 500;
+         let payCurrency = "UAH";
 
-        afterEach(() => {
-            refundPage.rejectRefund();
-        })
+         createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+         cy.wait(3000);
+         transactionsPage.createFullRefundUAHAndCheckAmount(payAmount, payCurrency);
+         refundPage.rejectRefund();
+     });
 
-})
+     it('Partial refund, payment currency matches the currencies that are in the merchant account', () => {
+
+         let payAmount = cy.getRandomArbitrary(100, 500);
+
+         for (let i = 0; i < refund.currency.length; i++) {
+             let payCurrency = refund.currency[i];
+             cy.log(payCurrency);
+             createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+             cy.wait(3000);
+             transactionsPage.createPartialRefundAndCheckAmount(payAmount, payCurrency);
+             refundPage.rejectRefund();
+         }
+     });
+
+     it('Partial refund, payment currency does not match the currencies that are in the merchant account', () => {
+
+         let payAmount = cy.getRandomArbitrary(100, 500);
+         //let payAmount = 500;
+         let payCurrency = "UAH";
+
+         createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+         cy.wait(3000);
+         transactionsPage.createPartialRefundUAHAndCheckAmount(payAmount, payCurrency);
+         refundPage.rejectRefund();
+     });
+
+
+ });

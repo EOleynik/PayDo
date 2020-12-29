488cc57d2f970b5ec9cd7b1df120ba8e94bc977b
EOleynik <Malbox@i.ua>
2020-12-29 08:22:54 +0200

    new RefandMathAPI

--- cypress/integration/Merchant/APITests/RefandMathTestsAPI.js
+++ cypress/integration/Merchant/APITests/RefandMathTestsAPI.js
@@ -1,23 +1,20 @@
-import transactionsPage from "../../elements/TransactionsPage";
-import createCheckoutPage from "../../elements/CreateCheckoutPage";
-import refundPage from "../../elements/RefundPage";
-import refund from "../../fixtures/refund";
-
-cy.getRandomArbitrary = function getRandomArbitrary(min, max) {
-    return (Math.random() * (max - min) + min).toFixed(2);
-};
+import transactionsPage from "../../../pages/TransactionsPage";
+import createCheckoutPage from "../../../pages/CreateCheckoutPage";
+import refundPage from "../../../pages/RefundPage";
+import refund from "../../../fixtures/refund";
+import parentPage from "../../../pages/ParentPage";
 
  describe('Refund math', () => {
 
      it('Full refund, payment currency matches the currencies that are in the merchant account', () => {
 
-         let payAmount = cy.getRandomArbitrary(100, 500);
+         let payAmount = parentPage.getRandomArbitrary(100, 500);
          //let payAmount = 500;
 
          for (let i = 0; i < refund.currency.length; i++) {
              let payCurrency = refund.currency[i];
              cy.log(payCurrency);
-             createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+             createCheckoutPage.createCheckoutAPI(payAmount, payCurrency);
              cy.wait(3000);
              transactionsPage.createFullRefundAndCheckAmount(payAmount, payCurrency);
              refundPage.rejectRefund();
@@ -26,11 +23,11 @@ cy.getRandomArbitrary = function getRandomArbitrary(min, max) {
 
      it('Full refund, payment currency does not match the currencies that are in the merchant account', () => {
 
-         let payAmount = cy.getRandomArbitrary(100, 500);
+         let payAmount = parentPage.getRandomArbitrary(100, 500);
          //let payAmount = 500;
          let payCurrency = "UAH";
 
-         createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+         createCheckoutPage.createCheckoutAPI(payAmount, payCurrency);
          cy.wait(3000);
          transactionsPage.createFullRefundUAHAndCheckAmount(payAmount, payCurrency);
          refundPage.rejectRefund();
@@ -38,12 +35,12 @@ cy.getRandomArbitrary = function getRandomArbitrary(min, max) {
 
      it('Partial refund, payment currency matches the currencies that are in the merchant account', () => {
 
-         let payAmount = cy.getRandomArbitrary(100, 500);
+         let payAmount = parentPage.getRandomArbitrary(100, 500);
 
          for (let i = 0; i < refund.currency.length; i++) {
              let payCurrency = refund.currency[i];
              cy.log(payCurrency);
-             createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+             createCheckoutPage.createCheckoutAPI(payAmount, payCurrency);
              cy.wait(3000);
              transactionsPage.createPartialRefundAndCheckAmount(payAmount, payCurrency);
              refundPage.rejectRefund();
@@ -52,11 +49,11 @@ cy.getRandomArbitrary = function getRandomArbitrary(min, max) {
 
      it('Partial refund, payment currency does not match the currencies that are in the merchant account', () => {
 
-         let payAmount = cy.getRandomArbitrary(100, 500);
+         let payAmount = parentPage.getRandomArbitrary(100, 500);
          //let payAmount = 500;
          let payCurrency = "UAH";
 
-         createCheckoutPage.getCheckoutAPI(payAmount, payCurrency);
+         createCheckoutPage.createCheckoutAPI(payAmount, payCurrency);
          cy.wait(3000);
          transactionsPage.createPartialRefundUAHAndCheckAmount(payAmount, payCurrency);
          refundPage.rejectRefund();

111a7118b0a574cb299272823e9be39ef5a099fd
EOleynik <malbox@i.ua>
2021-12-29 10:08:01 +0200

    version 2

--- cypress/pages/ParentPage.js
+++ cypress/pages/ParentPage.js
@@ -1,16 +1,18 @@
 import card from "../fixtures/Stage/card.json";
 import feen from "../fixtures/Stage/feen.json";
-import betweenWallets from "../fixtures/Stage/betweenWallets.json";
+import betweenWallets from "../fixtures/Prod/betweenWallets.json";
 import * as lookup from "country-code-lookup";
 import merchants from "../fixtures/Prod/merchants.json";
-import rate_com from "../fixtures/Prod/rate_com.json";
 
 
 class ParentPage {
 
-    getLogin(email, password) {
+    getLogin(email, password, authenticator) {
         cy.get("[formcontrolname=email]").clear().type(email);
         cy.get("[formcontrolname=password]").clear().type(password);
+        cy.contains('span', ' Login ').click();
+        cy.wait(1000);
+        cy.get('[class="d-block"]').type(this.get2FACode(authenticator));
         cy.contains('span', ' Login ').click();
     }
 
@@ -19,6 +21,17 @@ class ParentPage {
         let secret = key;
         let code = notp.authenticator.generate(secret);
         return code
+    }
+
+    getIndex_max(arr) {
+        let i, maxV, maxP;
+        for( i = 0; i < arr.length; i++) {
+            if( typeof maxV === "undefined" || arr[i] > maxV ) {
+                maxV = arr[i];
+                maxP = i;
+            }
+        }
+        return maxP;
     }
 
     getRandomArbitrary = function getRandomArbitrary(min, max) {
@@ -111,6 +124,10 @@ class ParentPage {
         }
     }
 
+    isButtonExist(button_name) {
+        cy.contains(button_name).should('exist')
+    }
+
     getMenu(name) {
         return cy.contains(name);
     }
@@ -136,10 +153,11 @@ class ParentPage {
         cy.get("[formcontrolname=holderName]").type(card.cardholder);
     }
 
-    getTokenUser(user, email, password, authenticator) {
+    getTokenUser(user, email, password, authenticator, account_type) {
         cy.request('POST', 'https://account.paydo.com/v1/users/login', {
             email: email,
             password: password,
+            type: account_type
         }).then((response) => {
             expect(response).property('status').to.equal(206);
 
@@ -152,18 +170,25 @@ class ParentPage {
                 body: {
                     email: email,
                     password: password,
+                    type: account_type
                 }
             }).its('headers')
                 .then((res) => {
                     let headers = res
-                    cy.writeFile("cypress/fixtures/Prod/" + user + "_headers.json", res)
+                    if(account_type === 1) {
+                        cy.writeFile("cypress/fixtures/Prod/Helpers/" + user + "_personal_headers.json", res)
+                    } else {
+                        cy.writeFile("cypress/fixtures/Prod/Helpers/" + user + "_business_headers.json", res)
+                    }
                 })
         })
     }
 
-    getAvailableBalance(user, wallet) {
-        cy.readFile("cypress/fixtures/Prod/" + user + "_headers.json").then((data) => {
+    getAvailableBalance(user, account_type, wallet) {
+
+        cy.readFile("cypress/fixtures/Prod/Helpers/" + user + "_" + account_type +"_headers.json").then((data) => {
             let tok = data.token
+
             cy.request({
                 method: 'GET',
                 url: "https://account.paydo.com/v1/wallets/get-all-balances/" + merchants.main_currency,
@@ -174,95 +199,145 @@ class ParentPage {
                 expect(response).property('status').to.equal(200);
                 expect(response.body).property('data').to.not.be.oneOf([null, ""]);
 
-                cy.writeFile("cypress/fixtures/Prod/available " + wallet + " "  + user + ".json", {'available': response.body.data[wallet].available.actual})
+                cy.writeFile("cypress/fixtures/Prod/Helpers/available " + account_type + " "+ wallet + " "  + user + ".json",
+                    {'available': response.body.data[wallet].available.actual})
             })
         })
     }
 
-    getCommission(amount, currency, tr_type, mid, admin_token, payment_method) {
-        cy.request({
-            method: 'GET',
-            url: "https://admin.paydo.com/v1/instrument-settings/commissions/for-mid/" + tr_type + "/" + mid + "/" + payment_method,
-            headers: {
-                token: admin_token,
-            }
-        }).then((response) => {
-            expect(response).property('status').to.equal(200);
-            expect(response.body).property('data').to.not.be.oneOf([null, ""]);
+    getCommissionForTransfer(amount, currency, tr_type, mid, payment_method) {
 
-            let hasCurrency =!!response.body.data.value?.[currency]
+        cy.readFile("cypress/fixtures/Prod/Helpers/admin_business_headers.json").then((data) => {
+            let tok = data.token
 
-            if(hasCurrency) {
-                cy.writeFile("cypress/fixtures/Prod/com" + tr_type + "_type.json", {
-                    'currency': currency,
-                    'fixcom': response.body.data.value[currency][0],
-                    'perscom': ((amount / 100) * response.body.data.value[currency][1]),
-                    'strategy': response.body.data.strategy
-                })
-            } else {
+            cy.request({
+                method: 'GET',
+                url: "https://admin.paydo.com/v1/instrument-settings/commissions/for-mid/" + tr_type + "/" + mid + "/" + payment_method,
+                headers: {
+                    token: tok,
+                }
+            }).then((response) => {
+                expect(response).property('status').to.equal(200);
+                expect(response.body).property('data').to.not.be.oneOf([null, ""]);
 
-                this.getRate('EUR', currency, admin_token)
-                let fix = (response.body.data.value.EUR[0] * rate_com.rates).toFixed(2)
-                let pers = ((amount / 100) * response.body.data.value.EUR[1] * rate_com.rates).toFixed(2)
-                cy.writeFile("cypress/fixtures/Prod/com" + tr_type + "_type.json", {
-                    'currency': currency,
-                    'fixcom': fix,
-                    'perscom': pers,
-                    'strategy': response.body.data.strategy
-                })
-            }
+                let hasCurrency = !!response.body.data.value?.[currency]
+
+                if (hasCurrency) {
+                    cy.writeFile("cypress/fixtures/Prod/Helpers/commission_for_" + tr_type + "_type_" + currency + ".json", {
+                        'currency': currency,
+                        'fixcom': response.body.data.value[currency][0],
+                        'perscom': ((amount / 100) * response.body.data.value[currency][1]),
+                        'strategy': response.body.data.strategy
+                    })
+                } else {
+
+                    this.getRate('for_commission', 'EUR', currency)
+
+                    cy.readFile("cypress/fixtures/Prod/Helpers/rate_for_commission_" + currency + ".json").then((data) => {
+                        let rate = data.rates
+
+                        let fix = (response.body.data.value.EUR[0] * rate).toFixed(2)
+                        let pers = ((amount / 100) * response.body.data.value.EUR[1]).toFixed(2)
+
+                        cy.writeFile("cypress/fixtures/Prod/Helpers/commission_for_" + tr_type + "_type_" + currency + ".json", {
+                            'currency': currency,
+                            'fixcom': fix,
+                            'perscom': pers,
+                            'strategy': response.body.data.strategy
+                        })
+                    })
+                }
+            })
         })
     }
 
-    getBaseCommission(amount, currency, tr_type, mid, admin_token) {
-        cy.request({
-            method: 'GET',
-            url: "https://admin.paydo.com/v1/instrument-settings/commissions/for-mid/" + tr_type + "/" + mid,
-            headers: {
-                token: admin_token,
-            }
-        }).then((response) => {
-            expect(response).property('status').to.equal(200);
-            expect(response.body).property('data').to.not.be.oneOf([null, ""]);
+    getBaseCommission(amount, currency, tr_type, mid) {
 
-            let hasCurrency =!!response.body.data.value?.[currency]
+        cy.readFile("cypress/fixtures/Prod/Helpers/admin_business_headers.json").then((data) => {
+            let tok = data.token
 
-            if(hasCurrency) {
-                cy.writeFile("cypress/fixtures/Prod/com" + tr_type + "_type.json", {
-                    'currency': currency,
-                    'fixcom': response.body.data.value[currency][0],
-                    'perscom': ((amount / 100) * response.body.data.value[currency][1]).toFixed(2),
-                    'strategy': response.body.data.strategy
-                })
-            } else {
+            cy.request({
+                method: 'GET',
+                url: "https://admin.paydo.com/v1/instrument-settings/commissions/for-mid/" + tr_type + "/" + mid,
+                headers: {
+                    token: tok,
+                }
+            }).then((response) => {
+                expect(response).property('status').to.equal(200);
+                expect(response.body).property('data').to.not.be.oneOf([null, ""]);
 
-                this.getRate("com", 'EUR', currency, admin_token)
-                let fix = (response.body.data.value.EUR[0] * rate_com.rates).toFixed(2)
-                let pers = ((amount / 100) * response.body.data.value.EUR[1]).toFixed(2)
-                cy.writeFile("cypress/fixtures/Prod/com" + tr_type + "_type.json", {
-                    'currency': currency,
-                    'fixcom': fix,
-                    'perscom': pers,
-                    'strategy': response.body.data.strategy
-                })
-            }
+                let hasCurrency = !!response.body.data.value?.[currency]
+
+                if (hasCurrency) {
+                    cy.writeFile("cypress/fixtures/Prod/Helpers/commission_for_" + tr_type + "_type.json", {
+                        'currency': currency,
+                        'fixcom': response.body.data.value[currency][0],
+                        'perscom': ((amount / 100) * response.body.data.value[currency][1]).toFixed(2),
+                        'strategy': response.body.data.strategy
+                    })
+                } else {
+
+                    this.getRate("for_com", 'EUR', currency)
+
+                    cy.readFile("cypress/fixtures/Prod/Helpers/rate_for_com_" + currency + ".json").then((data) => {
+                        let rate = data.rates
+                        let fix = (response.body.data.value[currency][0] * rate).toFixed(2)
+                        let pers = ((amount / 100) * response.body.data.value[currency][1]).toFixed(2)
+                        cy.writeFile("cypress/fixtures/Prod/Helpers/commission_for_" + tr_type + "_type.json", {
+                            'currency': currency,
+                            'fixcom': fix,
+                            'perscom': pers,
+                            'strategy': response.body.data.strategy
+                        })
+                    })
+                }
+            })
         })
     }
 
-    getRate(type, from, to, admin_token) {
+    getAllAvailableBalances(user, account_type, wallets) {
 
-        cy.request({
-            method: 'GET',
-            url: "https://admin.paydo.com/v1/currencies/get-rates-for/" + from,
-            headers: {
-                token: admin_token,
-            }
-        }).then((response) => {
-            expect(response).property('status').to.equal(200);
-            expect(response.body).property('data').to.not.be.oneOf([null, ""]);
+        cy.readFile("cypress/fixtures/Prod/Helpers/" + user + "_" + account_type +"_headers.json").then((data) => {
+            let tok = data.token
 
-            cy.writeFile("cypress/fixtures/Prod/rate_" + type + ".json", {
-                'rates': response.body.data.rates[to]
+                cy.request({
+                    method: 'GET',
+                    url: "https://account.paydo.com/v1/wallets/get-all-balances/" + merchants.main_currency,
+                    headers: {
+                    token: tok
+                    }
+                }).then((response) => {
+                    expect(response).property('status').to.equal(200);
+                    expect(response.body).property('data').to.not.be.oneOf([null, ""]);
+
+                    cy.writeFile("cypress/fixtures/Prod/Helpers/all available " + account_type + " balances "  + user + ".json", [
+                        response.body.data[wallets[0]].available.actual,
+                        response.body.data[wallets[1]].available.actual,
+                        response.body.data[wallets[2]].available.actual,
+                        response.body.data[wallets[3]].available.actual])
+
+                })
+        })
+    }
+
+    getRate(type, from, to) {
+
+        cy.readFile("cypress/fixtures/Prod/Helpers/admin_business_headers.json").then((data) => {
+            let tok = data.token
+
+            cy.request({
+                method: 'GET',
+                url: "https://admin.paydo.com/v1/currencies/get-rates-for/" + from,
+                headers: {
+                    token: tok,
+                }
+            }).then((response) => {
+                expect(response).property('status').to.equal(200);
+                expect(response.body).property('data').to.not.be.oneOf([null, ""]);
+
+                cy.writeFile("cypress/fixtures/Prod/Helpers/rate_" + type + "_" + to +".json", {
+                    'rates': response.body.data.rates[to]
+                })
             })
         })
     }
@@ -279,6 +354,49 @@ class ParentPage {
         } else {
             cy.get('[class="mat-radio-container"]').eq(0).click()
         }
+    }
+
+
+    send2FA(email, password) {
+        cy.request('POST', 'https://account.paydo.com/v1/users/login', {
+            email: email,
+            password: password
+        }).then((response) => {
+            expect(response).property('status').to.equal(206);
+        })
+    }
+
+    resetPassword(email) {
+        cy.request('POST', 'https://account.paydo.com/v1/users/request-reset-password', {
+            email: email,
+
+        }).then((response) => {
+            expect(response).property('status').to.equal(200);
+        })
+    }
+
+    created(i) {
+        cy.request('POST', 'https://account.paydo.com/v1/users/create', {
+            "affiliateIdentifierOf": null,
+            "email": "eugeniy.o+testEmail_6" + i + "@payop.com",
+            "language": "en",
+            "password": "1q2w3e$R%T^Y",
+            projectId: 1,
+            type: "[Auth] Register User",
+            "userTypeRegistration": 2
+        }).then((response) => {
+            expect(response).property('status').to.equal(201);
+        })
+    }
+
+    getMaxAvailableBalanceAfterRecalculation(user, account_type, position) {
+
+        cy.readFile("cypress/fixtures/Prod/Helpers/all available " + account_type + " balances " + user + ".json").then((data) => {
+            let balances = data
+
+            cy.writeFile("cypress/fixtures/Prod/Helpers/max available " + account_type + " " + user + ".json",
+                {'available': balances[position], 'wallet':betweenWallets.wallets[position]})
+        })
     }
 
 

d5731c8f6ee6e73cc38531d212ca5385ae7231b9
EOleynik <malbox@i.ua>
2023-05-09 08:01:00 +0300

    PricingPage.js added

---
+++ cypress/pages/PricingPage.js
@@ -0,0 +1,34 @@
+import parentPage from "../pages/ParentPage";
+
+const sectionTitle = '.section-title';
+const noVerificationBlock = '.no-verification-block';
+
+class PricingPage {
+
+    checkUrl(Url) {
+        parentPage.checkUrl(Url)
+    }
+
+    checkPageTitle(title) {
+        parentPage.isPageTitleExist(sectionTitle);
+        parentPage.checkText(title, sectionTitle)
+    }
+
+    checkInfoText(checkText) {
+        parentPage.isElementExist(noVerificationBlock);
+        // parentPage.checkText(checkText, noVerificationBlock);
+        cy.get(noVerificationBlock).find('p').invoke('text').should((text) => {
+            expect(text).to.eq(checkText);
+        })
+    }
+
+    checkButtonExist(button_name) {
+        parentPage.isButtonExist(button_name);
+    }
+
+    checkButtonStatus(name, status) {
+        parentPage.getButtonStatus(name, status)
+    }
+}
+
+export default new PricingPage();
